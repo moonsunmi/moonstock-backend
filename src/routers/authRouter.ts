5415de81013618edd0988a3ea06a0a1eec3dd9fc
moonsunmi <celebmoon@naver.com>
2024-10-18 22:54:01 +0900

    feat: connect refresh token

--- src/routers/authRouter.ts
+++ src/routers/authRouter.ts
@@ -2,6 +2,7 @@ import {Router} from 'express'
 import multer from 'multer'
 import {Prisma, PrismaClient} from '@prisma/client'
 import * as U from '../utils'
+import {extractUserIdFromJwt} from '../utils'
 
 export const authRouter = (...args: any[]) => {
   const router = Router()
@@ -23,48 +24,47 @@ export const authRouter = (...args: any[]) => {
       } catch (e) {
         if (e instanceof Prisma.PrismaClientKnownRequestError) {
           if (e.code === 'P2002') {
-            res
-              .status(409)
-              .json({ok: false, errorMessage: '이미 가입한 회원입니다.'})
+            res.status(409).json({errorMessage: '이미 가입한 회원입니다.'})
           } else {
-            res.status(500).json({ok: false, errorMessage: e.message})
+            res.status(500).json({errorMessage: e.message})
           }
         } else {
-          res.status(500).json({ok: false, errorMessage: '서버 오류'})
+          res.status(500).json({errorMessage: '서버 오류'})
         }
       }
     })
     .post('/login', upload.none(), async (req, res) => {
       const {email, password} = req.body
       const result = await client.user.findUnique({where: {email: email}})
-      console.log(req.cookies)
 
       if (!result) {
-        res
-          .status(401)
-          .json({ok: false, errorMessage: '등록되지 않은 사용자입니다.'})
+        res.status(401).json({errorMessage: '등록되지 않은 사용자입니다.'})
         return
       }
 
       const {id: _id, password: _password, ...userInfo} = result
       const isSamePw = await U.comparePasswordP(password, result.password)
       if (isSamePw) {
-        const token = await U.jwtSignP({id: result.id})
+        const token = await U.jwtSignP({id: result.id}, {expiresIn: '1m'})
+        const refreshToken = await U.jwtSignP(
+          {id: result.id},
+          {expiresIn: '7d'}
+        )
 
-        res.cookie('token', token, {
+        // 쿠키값이 넘어가기는 하는데,(set-cookie) 프런트에서 다시 전달하지를 못하고 있음.
+        res.cookie('refreshToken', refreshToken, {
           httpOnly: true,
-          secure: process.env.NODE_ENV === 'production',
-          sameSite: 'strict',
+          secure: process.env.NODE_ENV1 === 'production',
+          // sameSite:none을 쓰려면 무조건 https(secure: true)를 적용해야
+          sameSite: process.env.NODE_ENV === 'production' ? 'none' : 'lax',
           maxAge: 24 * 60 * 60 * 1000
         })
         res.status(200).json({
-          ok: true,
+          token: token,
           userInfo: userInfo
         })
       } else {
-        res
-          .status(401)
-          .json({ok: false, errorMessage: '비밀번호가 틀렸습니다.'})
+        res.status(401).json({errorMessage: '비밀번호가 틀렸습니다.'})
       }
     })
     .post('/logout', async (req, res) => {
@@ -73,9 +73,21 @@ export const authRouter = (...args: any[]) => {
         expires: new Date(0)
       })
     })
-    .post('/refreshToken', async (req, res) => {
+    .post('/refresh-token', async (req, res) => {
       const {refreshToken} = req.cookies // from cookies??
+      if (!refreshToken) {
+        return res.status(401).json({errorMessage: 'no refresh token'})
+      }
       try {
+        const userId = extractUserIdFromJwt(refreshToken)
+
+        if (!userId) {
+          return res.status(401).json({
+            errorMessage: '유효하지 않은 토큰입니다. 다시 로그인해 주세요.'
+          })
+        }
+        const token = await U.jwtSignP({id: userId}, {expiresIn: '1m'})
+        return res.status(200).json({token: token})
       } catch (e) {}
     })
     .get('/profile', (req, res) => {
@@ -83,9 +95,7 @@ export const authRouter = (...args: any[]) => {
 
       console.log(token)
       if (!token) {
-        return res
-          .status(401)
-          .json({ok: false, errorMessage: '인증 토큰이 없습니다.'})
+        return res.status(401).json({errorMessage: '인증 토큰이 없습니다.'})
       }
 
       // try {
--- src/routers/usersRouter.ts
+++ src/routers/usersRouter.ts
@@ -9,34 +9,31 @@ export const usersRouter = (...args: any[]) => {
   return router
     .get('/holdings', async (req, res) => {
       try {
-        const token = req.cookies.token
+        const token = req.headers.authorization?.split('Bearer ')[1]
+        console.log(token)
         if (!token) {
-          return res
-            .status(401)
-            .json({ok: false, errorMessage: '로그인이 필요합니다.'})
+          return res.status(401).json({errorMessage: '로그인이 필요합니다.'})
         }
-
         const userId = extractUserIdFromJwt(token)
+
+        console.log(userId)
         if (!userId) {
           return res.status(401).json({
-            ok: false,
             errorMessage: '유효하지 않은 토큰입니다. 다시 로그인해 주세요.'
           })
         }
 
         const result = await client.holding.findMany({where: {userId: userId}})
 
-        res.status(200).json({ok: true, holdings: result})
+        res.status(200).json({holdings: result})
       } catch (err) {
         if (err instanceof Prisma.PrismaClientKnownRequestError) {
           res.json({
-            ok: false,
             errorMessage: '데이터베이스 오류가 발생했습니다.'
           })
         }
         console.error('알 수 없는 오류 발생:', err)
         res.status(500).json({
-          ok: false,
           errorMessage: '서버 오류가 발생했습니다. 나중에 다시 시도해 주세요.'
         })
       }
@@ -45,47 +42,41 @@ export const usersRouter = (...args: any[]) => {
       try {
         const token = req.cookies.token
         if (!token) {
-          return res
-            .status(401)
-            .json({ok: false, errorMessage: '로그인이 필요합니다.'})
+          return res.status(401).json({errorMessage: '로그인이 필요합니다.'})
         }
 
         const userId = extractUserIdFromJwt(token)
         if (!userId) {
           return res.status(401).json({
-            ok: false,
             errorMessage: '유효하지 않은 토큰입니다. 다시 로그인해 주세요.'
           })
         }
 
         const {holdingId} = req.params
         if (!holdingId) {
-          return res
-            .status(400)
-            .json({ok: false, errorMessage: 'holdingId가 필요합니다.'})
+          return res.status(400).json({errorMessage: 'holdingId가 필요합니다.'})
         }
 
         const result = await client.holding.findUnique({
-          where: {id: holdingId, userId: userId}
+          where: {id: holdingId, userId: userId},
+          include: {stock: true}
         })
+
         if (!result) {
           return res.status(404).json({
-            ok: false,
             errorMessage: '해당 주식 정보를 찾을 수 없습니다.'
           })
         }
 
-        res.status(200).json({ok: true, holding: result})
+        res.status(200).json({holding: result})
       } catch (err) {
         if (err instanceof Prisma.PrismaClientKnownRequestError) {
           res.json({
-            ok: false,
             errorMessage: '데이터베이스 오류가 발생했습니다.'
           })
         }
         console.error('알 수 없는 오류 발생:', err)
         res.status(500).json({
-          ok: false,
           errorMessage: '서버 오류가 발생했습니다. 나중에 다시 시도해 주세요.'
         })
       }
@@ -94,31 +85,23 @@ export const usersRouter = (...args: any[]) => {
       try {
         const token = req.cookies.token
         if (!token) {
-          return res
-            .status(401)
-            .json({ok: false, errorMessage: '로그인이 필요합니다.'})
+          return res.status(401).json({errorMessage: '로그인이 필요합니다.'})
         }
 
         const userId = extractUserIdFromJwt(token)
         if (!userId) {
           return res.status(401).json({
-            ok: false,
             errorMessage: '유효하지 않은 토큰입니다. 다시 로그인해 주세요.'
           })
         }
-
-        // const result {where}
-        // res.status(200).json({ok: true, holdings: result})
       } catch (err) {
         if (err instanceof Prisma.PrismaClientKnownRequestError) {
           res.json({
-            ok: false,
             errorMessage: '데이터베이스 오류가 발생했습니다.'
           })
         }
         console.error('알 수 없는 오류 발생:', err)
         res.status(500).json({
-          ok: false,
           errorMessage: '서버 오류가 발생했습니다. 나중에 다시 시도해 주세요.'
         })
       }
@@ -127,31 +110,23 @@ export const usersRouter = (...args: any[]) => {
       try {
         const token = req.cookies.token
         if (!token) {
-          return res
-            .status(401)
-            .json({ok: false, errorMessage: '로그인이 필요합니다.'})
+          return res.status(401).json({errorMessage: '로그인이 필요합니다.'})
         }
 
         const userId = extractUserIdFromJwt(token)
         if (!userId) {
           return res.status(401).json({
-            ok: false,
             errorMessage: '유효하지 않은 토큰입니다. 다시 로그인해 주세요.'
           })
         }
-
-        // const result
-        // res.status(200).json({ok: true, holdings: result})
       } catch (err) {
         if (err instanceof Prisma.PrismaClientKnownRequestError) {
           res.json({
-            ok: false,
             errorMessage: '데이터베이스 오류가 발생했습니다.'
           })
         }
         console.error('알 수 없는 오류 발생:', err)
         res.status(500).json({
-          ok: false,
           errorMessage: '서버 오류가 발생했습니다. 나중에 다시 시도해 주세요.'
         })
       }
@@ -160,41 +135,34 @@ export const usersRouter = (...args: any[]) => {
       try {
         const token = req.cookies.token
         if (!token) {
-          return res
-            .status(401)
-            .json({ok: false, errorMessage: '로그인이 필요합니다.'})
+          return res.status(401).json({errorMessage: '로그인이 필요합니다.'})
         }
 
         const userId = extractUserIdFromJwt(token)
         if (!userId) {
           return res.status(401).json({
-            ok: false,
             errorMessage: '유효하지 않은 토큰입니다. 다시 로그인해 주세요.'
           })
         }
 
         const {holdingId} = req.params
         if (!holdingId) {
-          return res
-            .status(400)
-            .json({ok: false, errorMessage: 'holdingId가 필요합니다.'})
+          return res.status(400).json({errorMessage: 'holdingId가 필요합니다.'})
         }
 
         const result = await client.holding.delete({
           where: {id: holdingId, userId: userId}
         })
 
-        if (result) return res.status(200).json({ok: true, holdings: result})
+        if (result) return res.status(200).json({holdings: result})
       } catch (err) {
         if (err instanceof Prisma.PrismaClientKnownRequestError) {
           res.json({
-            ok: false,
             errorMessage: '데이터베이스 오류가 발생했습니다.'
           })
         }
         console.error('알 수 없는 오류 발생:', err)
         res.status(500).json({
-          ok: false,
           errorMessage: '서버 오류가 발생했습니다. 나중에 다시 시도해 주세요.'
         })
       }
